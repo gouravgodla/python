3d4a0395a995b3a140bafeb19e26e7d22b35419f
gouravgodla <98866998+gouravgodla@users.noreply.github.com>
2024-09-13 02:42:20 +0530

    day-3

---
+++ day3.py
@@ -0,0 +1,9 @@
+# Day3 - Modules and Pip
+import pandas  # This is an example of external module
+import hashlib  # This is an example of built in module
+
+print("Hi gourav it day-3 of python ")
+
+# Dont worry about how to use these modules just yet!
+# pandas.read_csv("one.csv")
+# m = hashlib.sha256()
